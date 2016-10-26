b6332f58c213ecf2627d6424b0634a88e827d591
diligentyang <905407339@qq.com>
2016-10-26 20:31:37 +0800

    考虑再三，应该求出行数

--- js/markdown.js
+++ js/markdown.js
@@ -2,19 +2,15 @@
 	
 	var line=0;
 	
-	$(".left").keydown(function(e){
-		if(e.keyCode==13){
-			$(".right").append("~");
-		}
-	})
 	$(".left").keyup(function(){
 		//$(".right").html($(".left").html());
-		$(".right").html();
+		//htmlcontents = htmlcontents.replace(/#([^#]*)#+|#([^#]*)/g,"<h1>$2$1</h1>");
+		
 		var markcontents = $(".left").html();
-		var htmlcontents = markcontents.replace(/###([^#]*)####+|###([^#]*)/g,"<h3>$2$1</h3>");
-		htmlcontents = htmlcontents.replace(/##([^#]*)##+|##([^#]*)/g,"<h2>$2$1</h2>");
-		htmlcontents = htmlcontents.replace(/#([^#]*)#+|#([^#]*)/g,"<h1>$2$1</h1>");
-		htmlcontents = htmlcontents.replace(/<br>/g,"");
-		$(".right").html("<textarea>"+htmlcontents+"</textarea>");
+		
+		var contents = markcontents.split("<br>");
+		line = contents.length-1==0?1:contents.length-1;
+		
+		$(".right").html("<textarea>"+markcontents+"</textarea>");
  	})
 })
