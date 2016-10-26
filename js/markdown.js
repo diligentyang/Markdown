71a41fa89afc1e41ef68e51e11c76202344a2c27
diligentyang <905407339@qq.com>
2016-10-26 20:21:38 +0800

    test

--- js/markdown.js
+++ js/markdown.js
@@ -1,11 +1,20 @@
 ﻿$(document).ready(function(){
+	
+	var line=0;
+	
+	$(".left").keydown(function(e){
+		if(e.keyCode==13){
+			$(".right").append("~");
+		}
+	})
 	$(".left").keyup(function(){
 		//$(".right").html($(".left").html());
 		$(".right").html();
 		var markcontents = $(".left").html();
-		var htmlcontents = markcontents.replace(/#([^#]*)#+|#([^#]*)/m,"<h1>$2$1<\/h1>");
-		htmlcontents = htmlcontents.replace(/##([^#]*)##+|##([^#]*)/m,"<h2>$2$1<\/h2>");
-		htmlcontents = htmlcontents.replace(/###([^#]*)###+|###([^#]*)/m,"<h3>$2$1<\/h3>");
-		$(".right").html(htmlcontents);
+		var htmlcontents = markcontents.replace(/###([^#]*)####+|###([^#]*)/g,"<h3>$2$1</h3>");
+		htmlcontents = htmlcontents.replace(/##([^#]*)##+|##([^#]*)/g,"<h2>$2$1</h2>");
+		htmlcontents = htmlcontents.replace(/#([^#]*)#+|#([^#]*)/g,"<h1>$2$1</h1>");
+		htmlcontents = htmlcontents.replace(/<br>/g,"");
+		$(".right").html("<textarea>"+htmlcontents+"</textarea>");
  	})
 })
