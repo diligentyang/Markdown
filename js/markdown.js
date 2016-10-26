8966b77c22e307237cf48e0cab50caa36ed1b099
diligentyang <905407339@qq.com>
2016-10-26 19:23:49 +0800

    修改标题正则

--- js/markdown.js
+++ js/markdown.js
@@ -1,8 +1,11 @@
 ﻿$(document).ready(function(){
 	$(".left").keyup(function(){
 		//$(".right").html($(".left").html());
+		$(".right").html();
 		var markcontents = $(".left").html();
-		var htmlcontents = markcontents.replace(/#(.*)#/,"<h1>$1</h1>");
+		var htmlcontents = markcontents.replace(/#([^#]*)#+|#([^#]*)/m,"<h1>$2$1<\/h1>");
+		htmlcontents = htmlcontents.replace(/##([^#]*)##+|##([^#]*)/m,"<h2>$2$1<\/h2>");
+		htmlcontents = htmlcontents.replace(/###([^#]*)###+|###([^#]*)/m,"<h3>$2$1<\/h3>");
 		$(".right").html(htmlcontents);
  	})
 })
