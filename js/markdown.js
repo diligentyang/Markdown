1a546be65c657e2434a9631a9e4cd311b9130cde
diligentyang <905407339@qq.com>
2016-10-26 18:51:35 +0800

    正则测试

--- js/markdown.js
+++ js/markdown.js
@@ -1,5 +1,8 @@
 ﻿$(document).ready(function(){
 	$(".left").keyup(function(){
-		$(".right").html($(".left").html());
-	})
+		//$(".right").html($(".left").html());
+		var markcontents = $(".left").html();
+		var htmlcontents = markcontents.replace(/#(.*)#/,"<h1>$1</h1>");
+		$(".right").html(htmlcontents);
+ 	})
 })
