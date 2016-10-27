b97442b216e6dc64be2fd40ee4a20dcbb76acbc2
diligentyang <905407339@qq.com>
2016-10-27 13:32:20 +0800

    下雨了

--- js/markdown.js
+++ js/markdown.js
@@ -11,24 +11,31 @@
 		var contents = markcontents.split("<br>");
 		line = contents.length-1==0?1:contents.length-1;
 		
+		change(contents,line);
+		
 		$(".right").html("<textarea>"+markcontents+"</textarea>");
  	})
 	
 })
 
-function change(contents)
+function change(contents,line)
 {
+	var htmlcontents="";
 	var regular = [{
 			pattern: 'h2',
 			reg: /#{2}([^#]*)*/,
-			replacement: '<h2>$1</h2>';
+			replacement: '<h2>$1</h2>'
 		},{
 			pattern: 'h1',
 			reg: /#([^#]*)*/,
-			replacement: '<h1>$1</h1>';
+			replacement: '<h1>$1</h1>'
+		}];
+	for (var i=0; i<line; i++) {
+		for (var j=0; j<regular.length; j++) {
+			preg_rep(contents[i], regular[j]);
 		}
-	
-	]
+	}
+	return htmlcontents;
 }
 
 function preg_rep(content,regular)
